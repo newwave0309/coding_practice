0d9262b7acda9c586c08ed0671f18d10b1890388
Yongmin Park <yongminpark@Yongminui-MacBook-Pro.local>
2018-05-07 23:15:46 +0900

    Add 02-data-type01.py -learn int, float, str

---
+++ 02-data-type01.py
@@ -0,0 +1,17 @@
+#정수 int -integer
+print(1+1)
+#소수 float
+print(1.5+2.1)
+#문자 string
+print("hello")
+
+# print ("hello" + "1")
+print(int(4/2))
+print(4//2)
+
+#문자1
+print("1")
+#숫자1
+print(int("1"))
+
+print(int("1.5"))
